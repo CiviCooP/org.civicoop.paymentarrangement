baa82f033ddda5ea3baf05b22b69320daad7cb8c
Jaap Jansma <info@edeveloper.nl>
2014-10-31 09:47:55 +0100

    added payment arrangement to contact

--- CRM/Paymentarrangement/Config.php
+++ CRM/Paymentarrangement/Config.php
@@ -10,10 +10,20 @@ class CRM_Paymentarrangement_Config {
   
   private $payment_arrangement_details_field;
   
+  private $contact_payment_arrangement_group;
+  
+  private $contact_payment_arrangement_field;
+  
+  private $contact_payment_arrangement_details_field;
+  
   private function __construct() {
     $this->payment_arrangement_group = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'payment_arrangement'));
     $this->payment_arrangement_field = civicrm_api3('CustomField', 'getsingle', array('name' => 'payment_arrangement', 'custom_group_id' => $this->payment_arrangement_group['id']));
     $this->payment_arrangement_details_field = civicrm_api3('CustomField', 'getsingle', array('name' => 'payment_arrangement_details', 'custom_group_id' => $this->payment_arrangement_group['id']));
+    
+    $this->contact_payment_arrangement_group = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'contact_payment_arrangement'));
+    $this->contact_payment_arrangement_field = civicrm_api3('CustomField', 'getsingle', array('name' => 'payment_arrangement', 'custom_group_id' => $this->contact_payment_arrangement_group['id']));
+    $this->contact_payment_arrangement_details_field = civicrm_api3('CustomField', 'getsingle', array('name' => 'payment_arrangement_details', 'custom_group_id' => $this->contact_payment_arrangement_group['id']));
   }
   
   /**
@@ -36,6 +46,18 @@ class CRM_Paymentarrangement_Config {
   
   public function getPaymentArrangementDetailsField($key='id') {
     return $this->payment_arrangement_details_field[$key];
+  }
+  
+  public function getContactPaymentArrangementGroup($key='id') {
+    return $this->contact_payment_arrangement_group[$key];
+  }
+  
+  public function getContactPaymentArrangementField($key='id') {
+    return $this->contact_payment_arrangement_field[$key];
+  }
+  
+  public function getContactPaymentArrangementDetailsField($key='id') {
+    return $this->contact_payment_arrangement_details_field[$key];
   }
   
 }
